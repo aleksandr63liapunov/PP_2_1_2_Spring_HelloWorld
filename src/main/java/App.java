4ac4fd8169d1ffb12181accefeff2b0e41409910
aleksandr63liapunov <liapunov.sania@mail.ru>
2022-01-24 04:36:51 +0400

    first

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2= (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        boolean comparison=bean==bean2;
+        System.out.println(comparison);
+
+        Cat cat= (Cat) applicationContext.getBean("cat");
+        System.out.println(cat);
+        Cat cat1= (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1);
+        boolean comparison2=cat==cat1;
+        System.out.println(comparison2);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,13 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name="cat")
+   @Scope(value = "prototype")
+    public Cat getName() {
+        Cat cat=new Cat();
+        cat.setName("ignat");
+
+        return cat;
+    }
+
 }
